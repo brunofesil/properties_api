44981d168411c2adac13ff556dd80198e9ba0057
brunofesil <brfsilva@outlook.com>
2021-09-20 16:26:23 -0300

    Terminadas rotas CRUD

--- app/routes/property.js
+++ app/routes/property.js
@@ -12,13 +12,58 @@ router.post('/', async (req, res) => {
     await property.save();
     res.status(200).json(property);
   } catch (error) {
-    res.status(500).json({error: 'Problem to create new property.'})
+    res.status(500).json({error: 'Problem to create property.'});
   }
-})
+});
+
+/* GET property show. */
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    let property = await Property.findById(id);
+    res.json(property);
+  } catch (error) {
+    res.status(500).json({error: 'Problem to show property.'});
+  }
+});
 
 /* GET properties listing. */
-// router.get('/', async (req, res) => {
-//   res.send('respond with a resource');
-// });
+router.get('/', async (req, res) => {
+  try {
+    let properties = await Property.find({})
+    res.json(properties)
+  } catch (error) {
+    res.status(500).json({error: 'Problem to get properties.'});
+  }
+});
+
+/* GET property edit. */
+router.put('/:id', async (req, res) => {
+  const { type, area } = req.body;
+  const { id } = req.params;
+
+  try {
+    let property = await Property.findOneAndUpdate(
+      {_id: id},
+      { $set: { type: type, area: area }},
+      { upsert: true, 'new': true }
+    )
+    res.json(property)
+  } catch (error) {
+    res.status(500).json({error: 'Problem to edit properties.'});
+  }
+});
+
+/* GET property delete. */
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    let property = await Property.findById(id);
+    await property.delete();
+    res.json({message: 'Ok'}).status(204);
+  } catch (error) {
+    res.status(500).json({error: 'Problem to delete property.'});
+  }
+});
 
 module.exports = router;
