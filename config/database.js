fb0c8e197537d388595e04dc78f9f4027d3430d2
brunofesil <brfsilva@outlook.com>
2021-09-20 18:14:01 -0300

    Criada variavel de ambiente MONGO_URL p/ db connect

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/property')
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL)
   .then(() => console.log('connection succesful'))
   .catch((err) => console.error(err));
